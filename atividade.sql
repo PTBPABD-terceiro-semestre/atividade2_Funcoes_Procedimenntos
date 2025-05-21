f09f0e89a3292733e5f0a7c73dd5ce0bb34a0103
GiovannaKarolline <mennezes.giovanna@gmail.com>
2025-05-21 18:58:31 -0300

    Update atividade.sql

--- atividade.sql
+++ atividade.sql
@@ -1,3 +1,10 @@
+/*
+Questão 01. Crie um procedimento denominado salaryHistogram, que distribua as frequências dos salários dos Professores em intervalos (Histograma).
+
+O número de intervalos será calculado de acordo com o parâmetro de entrada do procedimento. Exemplo: EXEC dbo.salaryHistogram 5;
+
+*/
+
 create procedure dbo.salaryHistogram @intervaloN int
 as
 begin
